8b6d59f10f67d0115ea2a8e424eafd1166da8799
Mattia <prodev@proconsulting.it>
2024-07-08 08:46:00 +0200

    setup of project

---
+++ API/Context/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Context;
+
+public class DataContext : DbContext
+{
+    private DbContextOptions<DataContext> _options;
+    private IConfiguration _conf;
+
+    public DataContext(DbContextOptions<DataContext> options, IConfiguration configuration) : base(options)
+    {
+        _options = options;
+        _conf = configuration; 
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_conf.GetConnectionString("DefaultConnection")); 
+    }
+    
+    // Add your DbSets here
+    
+}
---
+++ API/Lib/DbManager.cs
@@ -0,0 +1,30 @@
+﻿using API.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Lib;
+
+public class DbManager
+{
+    public static void MigrateDatabase(IApplicationBuilder app)
+    {
+        int t = 0;
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
+
+        using DataContext context = scope.ServiceProvider.GetRequiredService<DataContext>();
+
+
+        while (!context.Database.CanConnect() && t <= 5)
+        {
+            Console.WriteLine("DB not ready...");
+            t += 1;
+            Thread.Sleep(5000);
+        }
+
+        if (context.Database.CanConnect())
+        {
+            context.Database.Migrate();
+        }
+
+        t = 0;
+    }
+}
